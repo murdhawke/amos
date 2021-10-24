d6aafc0fcc0ae368c673cb215b1174447aaeb9ee
murdhawke <amoscheruiyot22@gmail.com>
2021-10-24 13:06:23 +0300

    Fixed typos

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,7 +2,7 @@
   <Layout>
     <div class="hero container-inner mx-auto flex flex-col sm:flex-row justify-between py-16 mt-8">
       <div class="text-4xl font-bold w-full sm:w-3/5 text-center sm:text-left">
-        <div class="leading-tight">Product Support, Javascript Enthusiast, Tech Excellene</div>
+        <div class="leading-tight">Product Support, Javascript Enthusiast, Tech Excellence</div>
         <div class="text-green-700 leading-tight">I love tech!</div>
       </div>
       <div class="mt-8 sm:mt-0">
